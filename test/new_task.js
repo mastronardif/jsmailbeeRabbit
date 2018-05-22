acf8b2f16469fa2e7beda2a5687567dd6ad1753e
mastronardif <mastronardif@gmail.com>
2018-05-21 22:20:31 -0400

    testing workers and new tasks.

--- test/new_task.js
+++ test/new_task.js
@@ -7,14 +7,34 @@ amqp.connect(CLOUDAMQP_URL, function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'task_queue';
     var msg = process.argv.slice(2).join(' ') || "Hello World!";
+    var event = new Date(Date.now());
+    var now = event.toISOString()
+    msg = now+': '+msg;
 
     ch.assertQueue(q, {durable: true});
     ch.sendToQueue(q, new Buffer(msg), {persistent: true});
     console.log(" [x] Sent '%s'", msg);
 
-    ch.sendToQueue(q, new Buffer(msg+".aaa"), {persistent: true});
-    ch.sendToQueue(q, new Buffer(msg+"..bbb"), {persistent: true});
-    ch.sendToQueue(q, new Buffer(msg+"...ccc"), {persistent: true});
+    mySend(ch, q, 'TEST a ');
+    //mySend(ch, q, 'TEST b ');
+    //mySend(ch, q, 'TEST c ');
+
+    setInterval(function() {
+        mySend(ch, q, 'TEST z ');
+      }, 3000);
   });
+
   setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
+console.log('++++++++++++++++++++++++++++++');
+var cnt=1;
+function mySend(ch, q, msg) {
+    //var msg = "mySend:";
+    var event = new Date(Date.now());
+    var now = event.toISOString()
+    var ts = event.toLocaleTimeString()+' | '+event.getUTCMilliseconds();
+    msg = '[' + cnt++ +'] '+now+'| '+msg;
+
+    ch.sendToQueue(q, new Buffer(msg+".aaa"), {persistent: true});
+
+}
